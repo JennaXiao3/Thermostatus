c1165ab23fdfadb15d5be7768fae412b4334e9ee
connor-li <77080536+connor-li@users.noreply.github.com>
2022-01-15 13:37:56 -0500

    set interval geolocation

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,9 +1,45 @@
 import { CurrentRenderContext } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 
 export const HomeScreen = ({navigation}, props) => {
+
+    const [positionNow, setPositionNow] = useState(null);
+    const [ watchPosition, setWatchPosition ] = useState(null);
+    const [ currentTime, setCurrentTime ] = useState('now');
+ 
+    //geolocation upon mounting
+    useEffect(
+        () => {
+            Geolocation.getCurrentPosition((position) => {
+                setInterval(() => {
+                    setPositionNow(() => position.coords);
+                    setCurrentTime(() => Date.now);
+                }, 10000);
+                
+            }, (error) => {
+                console.log(
+                    `This is why location isn't working: ${error.code}, ${error.message}`
+                )
+            });
+
+            Geolocation.watchPosition(
+                (position) => {
+                    setWatchPosition(() => position.coords);
+                }, (error) => {
+                    console.log('rip')
+                }
+            );
+        }, 
+    []);
+
+    const handlePress = () => {
+        console.log(currentTime);
+    }
+
+
     return(
         <View style={styles.screenContainer}>
             <Text>CONGRATS THIS APP FINALLY WORKS</Text>
@@ -12,6 +48,8 @@ export const HomeScreen = ({navigation}, props) => {
             <TouchableOpacity onPress={() => navigation.navigate('managehomes')}>
                 <Text>Go to Manage Homes</Text>
             </TouchableOpacity>
+
+            <Button onPress={handlePress}/>
             <StatusBar style="auto" />
       </View>
     );
